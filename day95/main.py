380e6fc2f03342e5631bef06bc798087dcb06ed6
chartb <74368960+chartb-tw@users.noreply.github.com>
2022-04-01 06:16:13 +0100

    update day95/main.py

--- day95/main.py
+++ day95/main.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 def home(): 
 	return render_template("homepage.html", rn = dt.datetime.now())
 
+# location line up - docs at https://www.realtimetrains.co.uk/about/developer/pull/docs/locationlist/
 @app.route('/<string:train_loc>')
 def station_dep(train_loc): 
 	resp = requests.get(url = f"{endp}json/search/{train_loc}", auth = api_cred)
@@ -20,7 +21,21 @@ def station_dep(train_loc):
 	dep_services = resp.json()
 	return render_template("stations.html", rn = dt.datetime.now(), services = dep_services["services"], station_name = dep_services["location"]["name"])
 
+# service info - docs at https://www.realtimetrains.co.uk/about/developer/pull/docs/serviceinfo/
+@app.route('/train/<serviceUid>/<year>/<month>/<day>')
+def train_detail(serviceUid, year, month, day):
+	resp = requests.get(f"{endp}json/service/{serviceUid}/{year}/{month}/{day}", auth = api_cred)
+	# resp.raise_for_status()
+	return jsonify(resp.json()) # Could have built this into a page, may do so at another date
 
+@app.route('/train/<serviceUid>')
+def train_detail_now(serviceUid):
+	rnow = dt.datetime.now()
+	return redirect(url_for('train_detail', serviceUid = serviceUid, year = rnow.strftime('%Y'), month = rnow.strftime('%m'), day = rnow.strftime('%d')))
+
+@app.route('/train')
+def cba_to_implement():
+	return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
